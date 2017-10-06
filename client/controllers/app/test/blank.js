905d58892762501660bc073bc3b7c92233190d83
Balakumar <Balakumar.thirunavukarasu@flex.com>
2017-10-06 03:24:20 -0700

    [Studio] Add screen "BLANK"

---
+++ client/controllers/app/test/blank.js
@@ -0,0 +1,5 @@
+angular.module('app').controller('app_test_blank', app_test_blank);
+function app_test_blank($scope, app) {
+    'use strict';
+    app.init($scope);
+}
--- client/states.js
+++ client/states.js
@@ -33,5 +33,14 @@ angular.module('app').config(function ($stateProvider) {
                 }
             }
         }
+    }).state('app.test/blank', {
+        views: {
+            app: {
+                controller: 'app_test_blank',
+                templateProvider: function (app) {
+                    return app.templateProvider('app.test.blank');
+                }
+            }
+        }
     });
 });
